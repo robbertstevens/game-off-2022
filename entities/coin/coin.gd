2a1522867f77cd0aaeb0e73cd26c554f51677ad8
Robbert Stevens <robbertstevens@gmail.com>
2022-11-05 19:55:57 +0100

    feat: added coin to group and start at random frame

--- entities/coin/coin.gd
+++ entities/coin/coin.gd
@@ -2,7 +2,13 @@ extends Area2D
 
 signal coin_picked_up
 
-func _on_body_entered(body: Node2D) -> void:
+@onready var animation := $AnimatedSprite2D
+
+func _ready() -> void:
+    animation.frame = randi_range(0, animation.frames.get_frame_count(animation.animation))
+
+
+func _on_body_entered(_body: Node2D) -> void:
     # TODO: Play sound
     emit_signal("coin_picked_up")
     queue_free()
